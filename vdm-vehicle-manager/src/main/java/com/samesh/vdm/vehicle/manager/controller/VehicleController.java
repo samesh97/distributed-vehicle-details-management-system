79f8bd957d3e795f7834729649221ec74ca84ea7
samesh97 <Alahakoon123@>
2022-04-09 15:05:00 +0530

    Fix endpoint 404 not found bug

--- vdm-vehicle-manager/src/main/java/com/samesh/vdm/vehicle/manager/controller/VehicleController.java
+++ vdm-vehicle-manager/src/main/java/com/samesh/vdm/vehicle/manager/controller/VehicleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "services/vehicle")
+@RequestMapping(value = "/vehicle")
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class VehicleController {
 
